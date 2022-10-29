c482e62fcc535fb8c14850e136587bf5c161cc1e
HurasAdam <huradam@wp.pl>
2022-10-30 00:58:02 +0200

    commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const playerPointsSpan = document.querySelector('.player-points');
+const computerPointsSpan = document.querySelector('.computer-points');
+const optionsButtons= document.querySelectorAll('.options button')
+const playerChoicespan= document.querySelector('.player-choice');
+const computerChoicespan= document.querySelector('.computer-choice');
+const resultText = document.querySelector('.result-text');
 const resetGameButton= document.querySelector('.reset-game')
